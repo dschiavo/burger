f52c15d349af10133c2a011d59c4a911bfd30e70
RBG <dschiavo.262@gmail.com>
2019-01-06 18:25:23 -0500

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon double cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger with avocado');
+INSERT INTO burgers (burger_name) VALUES ('Burger with lettuce and tomato');
