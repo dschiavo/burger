1c058c2d84141656e7a5d7704c0e3fe9d08b0ee3
RBG <dschiavo.262@gmail.com>
2019-01-07 23:37:57 -0500

    more files

--- models/burger.js
+++ models/burger.js
@@ -1,47 +1,29 @@
-$(function() {
-    $(".change-devour").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevour = $(this).data("newdevour");
-  
-      var newDevourState = {
-        devoured: newDevour
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevourState
-      }).then(
-        function() {
-          console.log("changed devoured state to", newDevour);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+// Import the ORM to implement functions that will interact with the database
+var orm = require('../config/orms.js');
+
+// Create the burger object
+var burger = {
+  // Select all burger table entries
+  selectAll: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
     });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#ca").val().trim(),
-        devoured: $("[name=false]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+  },
+
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
     });
-  
-   
-  });
-  
+  },
+
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
